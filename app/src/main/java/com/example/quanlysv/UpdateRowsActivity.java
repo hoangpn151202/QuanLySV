6039b5bc620222d6d1c4c7e298bb3d263f9ebfac
hoangpn151202 <104040979+hoangpn151202@users.noreply.github.com>
2022-11-11 19:25:30 +0700

    c

---
+++ app/src/main/java/com/example/quanlysv/UpdateRowsActivity.java
@@ -0,0 +1,55 @@
+package com.example.quanlysv;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+
+public class UpdateRowsActivity extends AppCompatActivity {
+    static ListView listView ;
+    ArrayList<UserModel> users=new ArrayList<>();
+    static CustomListAdapterUpdateRows updateAdapter;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_rows);
+
+        try {
+            UsersDatabaseAdapter usersDB = new UsersDatabaseAdapter(getApplicationContext());
+            users = usersDB.getRows();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        updateAdapter = new CustomListAdapterUpdateRows(this, users);
+        listView = (ListView) findViewById(R.id.listupdateviewID);
+        listView.setAdapter(updateAdapter);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Update User");
+        }
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/quanlysv/activity_update_rows.java
@@ -0,0 +1,14 @@
+package com.example.quanlysv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_update_rows extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_rows);
+    }
+}
